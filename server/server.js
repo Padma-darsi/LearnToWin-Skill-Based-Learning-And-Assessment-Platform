220fc4da7d40a9ce9f3b80920f866fdb21001cd3
Padma Darsi <padmadarsi230@gmail.com>
2026-01-23 20:58:43 +0530

    edited some files

--- server/server.js
+++ server/server.js
@@ -19,27 +19,23 @@ dotenv.config();
 
 const app = express();
 
- app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
+ import cors from "cors";
 
-      if (
-        origin.startsWith("http://localhost:") ||
-        origin.endsWith(".vercel.app")
-      ) {
-        return callback(null, true);
-      }
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); // Postman, mobile apps
+    if (
+      origin.startsWith("http://localhost:") ||
+      origin.endsWith(".vercel.app")
+    ) return callback(null, true);
 
-      callback(new Error("Not allowed by CORS"));
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-  })
-);
+    callback(new Error("Not allowed by CORS"));
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
-app.options("/", cors());
 
 
 app.use(express.json());
