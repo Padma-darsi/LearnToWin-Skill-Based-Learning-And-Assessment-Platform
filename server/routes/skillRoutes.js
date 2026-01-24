b97ef77327ebc2534103f61faf8f851067098977
Padma Darsi <padmadarsi230@gmail.com>
2026-01-24 14:54:22 +0530

    edited server files

--- server/routes/skillRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getSkillTree } from "../controllers/skillController.js";
-
-const router = express.Router();
-
-router.get("/tree", getSkillTree);
-
-export default router;
